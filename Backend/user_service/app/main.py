4a07479ea541d81bb3c19eb9cbb0a4a610933bc5
me-Afzal <afzalkottukkal23@gmail.com>
2025-10-19 16:47:43 +0530

    API versioaing added

--- Backend/user_service/app/main.py
+++ Backend/user_service/app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from app.api.v1.routes import router as v1_router
-
+from prometheus_fastapi_instrumentator import Instrumentator
 app = FastAPI(
     title="CVAlyze User Service API",
     version="1.0.0"
 )
+
+# Prometheus scrape metrics
+Instrumentator().instrument(app).expose(app)
 
 # Include versioned router
 app.include_router(v1_router, prefix="/api/v1")
